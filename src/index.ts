5df96ace4fc848aaea85cd5a299fe120ea954941
And111rey <samu111ray@gmail.com>
2023-11-08 16:44:18 +0200

    gitignore

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!))))Hello')
+  res.send('Hello World!))))Hello new')
 })
 
 app.listen(port, () => {
